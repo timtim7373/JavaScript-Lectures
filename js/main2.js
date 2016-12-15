dc478d43e70f0ae4fea9cc4e449056eccfd9b8a0
Oksana <kol.oksana81@gmail.com>
2016-12-15 21:28:41 +0200

    Lection 2c

---
+++ js/main2.js
@@ -0,0 +1,32 @@
+// console.log("2+2*2=", 2+2*2);  //6
+// console.log("2+2=", 2+2); //
+// console.log("5-3", 5-3); //2
+// console.log("5*3", 5*3); //15
+// console.log("5%3=", 5%3); //2
+// var x=25;
+// console.log("-x=", -x); //-25
+
+// var y = '25';
+// console.log("y=", y); //string '25'
+// console.log("typeof y=", typeof y); // string
+// console.log("+y=",+y); // number 25
+// console.log("typeof y=", typeof +y); //number
+
+// var z = '10';
+// z = +z;
+// console.log("typeof z=", typeof z); //number
+
+// console.log("5>3", 5>3); //true
+// console.log("5<3", 5<3); //false
+// console.log("3>=3", 3>=3); //true
+// console.log("3<=5", 3<=5); //true
+// console.log("5 != '5' =", 5 != '5'); //false
+// console.log("5 !== '5' =", 5 !== '5'); //true
+// console.log("null == undefined =", null == undefined); // true
+// console.log("null === undefined =", null === undefined); // false
+
+function test () {
+	return "test susses!";
+}
+
+console.log(true && test() && "test" && "end og ends list"); // end
