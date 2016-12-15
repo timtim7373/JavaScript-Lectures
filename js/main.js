d1353cd0543225ce3eb48a675ff612baff2e1545
Oksana <kol.oksana81@gmail.com>
2016-12-15 21:28:11 +0200

    Lection2a

--- js/main.js
+++ js/main.js
@@ -1,43 +1,99 @@
-window.jQuery
-"use strict";
-// alert("Hello World");
-// console.log("Hello worldsss");
-// debugger;
-// var x = "My x";
-// var x = "My x";
-// x = "Another";
+// console.log("2+2*2=", 2+2*2);  //6
+// console.log("2+2=", 2+2); //
+// console.log("5-3", 5-3); //2
+// console.log("5*3", 5*3); //15
+// console.log("5%3=", 5%3); //2
+// var x=25;
+// console.log("-x=", -x); //-25
 
-// var x = undefined;
-// console.log(typeof x);
+// var y = '25';
+// console.log("y=", y); //string '25'
+// console.log("typeof y=", typeof y); // string
+// console.log("+y=",+y); // number 25
+// console.log("typeof y=", typeof +y); //number
 
-// var x = null;
-// console.log(typeof x);
+// var z = '10';
+// z = +z;
+// console.log("typeof z=", typeof z); //number
 
-// var x = {
-// key1: "value1",
-// key2: "value2",
-// "key3": "value3"
-// };
-// x.key1 = "new value";
-// x["key2"] = "new value 2";
-// console.log(typeof x);
+// console.log("5>3", 5>3); //true
+// console.log("5<3", 5<3); //false
+// console.log("3>=3", 3>=3); //true
+// console.log("3<=5", 3<=5); //true
+// console.log("5 != '5' =", 5 != '5'); //false
+// console.log("5 !== '5' =", 5 !== '5'); //true
+// console.log("null == undefined =", null == undefined); // true
+// console.log("null === undefined =", null === undefined); // false
 
-// var x = ["element","element2",25, {},[],null];
-// console.log(x[0]);
-// x[2]=44;
-// console.log(x[2]);
-// console.log(typeof x);
-// Array.isArray(x); // true
+// function test () {
+	// return "test susses!";
+// }
 
-// var x function () {};
-// x.key1 = "test";
-// console.log(x.key1); //"test";
-// console.log(typeof x); //"function";
+// console.log(true && test() && "test" && "end of ends list"); // end of ends list
+// console.log(true && test() && 0 && "end of ends list"); //0
+// console.log(false && 6); // false
 
-var x = ["123"];
-console.log(x); // ["123"];
-var y = x;
-console.log(y); // ["123"];
-y[0]= 345;
-console.log(y); // ["345"];
-console.log(x); // ["345"];
+// console.log (false || 6); // 6
+// console.log (true || 6); // true
+// console.log (false || "end" || 0); //"end"
+// console.log ("start" || true || 0) // "start"
+
+// consle.log("!0 =", !0) //true
+// consle.log('!"" =', !"") //true
+// console.log('!"true because contains text"', !"true because contains text"); // false
+
+// var x = (3,4,5);
+// console.log(x); //5
+
+// var x = 10, y = 5, z = x + y;
+// console.log (z);
+// console.log ("undefined + 25", undefined + 25); // NaN
+// console.log ("NaN==NaN", NaN==NaN); //false
+// console.log ("NaN===NaN", NaN===NaN); // false
+// console.log ("typeof NaN", typeof NaN); // number
+// console.log ("2*(2+3)", 2*(2+3)); // 10
+
+// var x=10; 
+// x++; //10
+// console.log(x); //11
+// console.log(x++); //11
+
+// var y=10; 
+// ++y; //11
+// console.log(y); //11
+// console.log(++y); //12
+
+// var x=10; 
+// x--; //10
+// console.log(x); //9
+// console.log(x--); //9
+
+// var y=10; 
+// --y; //9
+// console.log(y); //9
+// console.log(--y); //8
+
+// console.log("'x'>'y'", 'x'>'y'); //false
+// console.log("'x'>'Y'", 'x'>'Y'); //true
+// console.log("'test' > 'tes' =", 'test' > 'tes'); //true
+// console.log("'test' > 'zes' =", 'test' > 'zes'); //false
+// console.log("'1' > '2' =", '1' > '2'); //false
+// console.log("'12' > '2' =", '12' > '2'); //false
+// console.log("+'12' > +'2' =", +'12' > +'2'); //true
+
+// console.log("'2' > 1 =", '2' > 1); // true
+// console.log("'1.0' == 1 =", '1.0' == 1); //true
+// console.log("true == 1", true == 1); //true
+// console.log("false == 0 && false == '' =", false == 0 && false == '' ="); // true
+
+// true == !"", !0, !false, !null, !undefined, !NaN, function () {};
+// false == "", 0, false, null, undefined, NaN;
+
+// if (true) {}
+
+// var x='25';
+// switch (x)
+// {
+	// case "text": console.log("Action 1");
+	// break;
+// }
